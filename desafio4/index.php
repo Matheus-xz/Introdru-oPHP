d29e27d44544fdbb5a9c965bfce410192280ffd7
Matheus Vieira <matheus.vfreire100@gmail.com>
2025-11-25 17:22:23 -0300

    atualização 2

--- desafio4/index.php
+++ desafio4/index.php
@@ -12,13 +12,22 @@
     </header>
     <main>
         <?php
+
+            $inicio = date("m-d-Y", strtotime("-7 days"));
+            $fim = date("m-d-Y");
+            $url ='https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\''.$inicio.'\'&@dataFinalCotacao=\''.$fim.'\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
+
+            $dados = json_decode(file_get_contents($url), true);
+
             $real = $_GET["valor"];
-            $valorDolar = 5.25;
+            $valorDolar = $dados["value"][0]["cotacaoCompra"];
             $valorConvertido = $real / $valorDolar;
 
             $padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
 
             echo "<p>O seu valor em real ". numfmt_format_currency($padrao, $real, "BRL") . " convertido em dólar são <strong>" . numfmt_format_currency($padrao, $valorConvertido, "USD<strong>". "</p>");
+
+            echo "<p>A cotação atual é ". $valorDolar . " Baseado no <strong> Banco Central <strong></p>"
         ?>
         <p><a href="index.html">Voltar</a></p> 
     </main>
